e3c4a6056e49bb85ae5b907b5a24b4b76fddaa01
ray247k <ray247k@gmail.com>
2022-01-09 11:20:23 +0800

    Add database connection

--- V1/controllers/channelController.go
+++ V1/controllers/channelController.go
@@ -1,11 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 
-	ChannelEntity "Osheet-api/V1/models/channel"
+	service "Osheet-api/v1/services"
 )
 
 type ChannelController struct{}
@@ -18,48 +19,12 @@ type ChannelController struct{}
 // @Success 	200 {object} []ChannelEntity.Channel
 // @Router 		/api/v1/channels [get]
 func (c ChannelController) Index(context *gin.Context) {
-	// company := context.Query("company")  // get query string: company
+	// company := context.Query("company")  // TODO get query string: company
 
-	ina := ChannelEntity.Channel{
-		TwitterAccount: "ninomaeinanis",
-		Name:           "Ninomae Ina'nis",
-		Avatar:         "https://pbs.twimg.com/profile_images/1473599637751693314/k_ZTGRX4_400x400.jpg",
-		Company:        "Hololive",
-		Unit:           "hololive English - Myth",
-		ChannelUrl:     "https://www.youtube.com/channel/UCMwGHR0BTZuLsmjY_NT5Pwg",
-		Height:         157,
-		Birthday:       "05/20",
-		DebutDate:      "2020/09/13",
-		Info: ChannelEntity.Info{
-			FanName: "takodachi tentacult",
-			Hashtag: ChannelEntity.Hashtag{
-				StreamTag: "#TAKOTIME #タコタイム",
-				FanArt:    "#inART #いなート",
-			},
-		},
+	channels, err := service.GetAllChannel()
+	if err != nil {
+		fmt.Println("Get /api/v1/channels Failed:", err)
 	}
-
-	ame := ChannelEntity.Channel{
-		TwitterAccount: "watsonameliaEN",
-		Name:           "Watson Amelia",
-		Avatar:         "https://pbs.twimg.com/profile_images/1318958836120649728/7JHxy2UO_400x400.jpg",
-		Company:        "Hololive",
-		Unit:           "hololive English - Myth",
-		ChannelUrl:     "https://www.youtube.com/channel/UCyl1z3jo3XHR1riLFKG5UAg",
-		Height:         150,
-		Birthday:       "01/06",
-		DebutDate:      "2020/09/13",
-		Info: ChannelEntity.Info{
-			FanName: "teamates",
-			Hashtag: ChannelEntity.Hashtag{
-				StreamTag: "#amelive",
-				FanArt:    "#ameliaRT",
-			},
-		},
-	}
-
-	var channels []ChannelEntity.Channel
-	channels = append(channels, ina, ame)
 
 	context.JSON(http.StatusOK, channels)
 }
@@ -76,24 +41,12 @@ func (c ChannelController) Index(context *gin.Context) {
 func (c ChannelController) Show(context *gin.Context) {
 	twitterAccount := context.Param("twitterAccount") // get URL path parameter: twitterAccount
 
-	channel := ChannelEntity.Channel{
-		TwitterAccount: twitterAccount,
-		Name:           "Garw Gura",
-		Avatar:         "https://pbs.twimg.com/profile_images/1309957523089354760/uRrxAmOB_400x400.jpg",
-		Company:        "Hololive",
-		Unit:           "hololive English - Myth",
-		ChannelUrl:     "https://www.youtube.com/channel/UCoSrY_IQQVpmIRZ9Xf-y93g",
-		Height:         141,
-		Birthday:       "06/20",
-		DebutDate:      "2020/09/13",
-		Info: ChannelEntity.Info{
-			FanName: "chumbuds",
-			Hashtag: ChannelEntity.Hashtag{
-				StreamTag: "#gawrgura",
-				FanArt:    "#gawrt",
-			},
-		},
+	channel, err := service.GetChannelByTwitterAccount(twitterAccount)
+	if err != nil {
+		fmt.Println("Get /api/v1/channels/{twitterAccount} Failed:", err)
 	}
 
 	context.JSON(http.StatusOK, channel)
 }
+
+// TODO: Create channel
