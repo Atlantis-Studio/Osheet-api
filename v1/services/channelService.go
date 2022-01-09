976f9e2049ef79935810a970e61fd16cce73befb
ray247k <ray247k@gmail.com>
2022-01-09 11:27:32 +0800

    Refactor channelService

--- v1/services/channelService.go
+++ v1/services/channelService.go
@@ -1,12 +1,14 @@
-package service
+package services
 
 import (
 	"Osheet-api/database"
 	"Osheet-api/v1/models"
 )
 
+type ChannelService struct{}
+
 // Get all channels
-func GetAllChannel() (channelList []*models.Channel, err error) {
+func (s ChannelService) GetAllChannel() (channelList []*models.Channel, err error) {
 	if err = database.GetDB().Find(&channelList).Error; err != nil {
 		return nil, err
 	}
@@ -15,7 +17,7 @@ func GetAllChannel() (channelList []*models.Channel, err error) {
 }
 
 // Get channel by twiiter account
-func GetChannelByTwitterAccount(twitterAccount string) (channel *models.Channel, err error) {
+func (s ChannelService) GetChannelByTwitterAccount(twitterAccount string) (channel *models.Channel, err error) {
 	if err = database.GetDB().Where("twitter_account = ?", twitterAccount).First(&channel).Error; err != nil {
 		return nil, err
 	}
